e621fc1076004e93ec7c6377ebea8e473656d532
wass <wassim.filali@gmail.com>
2020-11-07 20:24:44 +0100

    initial move commit

---
+++ src/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "stm32f4xx_hal.h"
+
+#define LED_PIN                                GPIO_PIN_13
+#define LED_GPIO_PORT                          GPIOC
+
+#endif // MAIN_H
